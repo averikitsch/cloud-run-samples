b421f2581a4ccd3668999b101e8b5f52e0986b0d
averikitsch <akitsch@google.com>
2020-06-04 13:41:20 -0700

    fix

--- helloworld-shell/script.sh
+++ helloworld-shell/script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +15,6 @@
 
 # [START run_helloworld_shell]
 
-#!/bin/bash
 set -e
 echo "Hello ${NAME:-World}!"
 
